60c090174ef6b854ba0cf1dca4620ea64b928c33
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-05 22:24:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,7 +45,7 @@ async def start(client, message: pyrogram.types.Message):
         await asyncio.sleep(2)
         await m.delete()
         await message.reply_photo(
-            photo=random.choice(START_IMAGE_URL),
+            photo=START_IMAGE_URL,
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
@@ -79,7 +79,7 @@ async def start(client, message: pyrogram.types.Message):
         await asyncio.sleep(2)
         await m.delete()
         await message.reply_photo(
-            photo=random.choice(START_IMAGE_URL),
+            photo=START_IMAGE_URL,
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
