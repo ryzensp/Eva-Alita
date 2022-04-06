a836d0516921b528e529adec5cd46eba13a308ae
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-06 10:50:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1094,7 +1094,7 @@ async def auto_filter(client, msg: pyrogram.types.Message, spoll=False):
 
     await message.reply_chat_action("Typing")
     m=await message.reply_sticker("CAACAgUAAx0CQTCW0gABB5EUYkx6-OZS7qCQC6kNGMagdQOqozoAAgQAA8EkMTGJ5R1uC7PIECME") 
-    await asyncio.sleep(2)
+    await asyncio.sleep(4)
     await m.delete()
     
 
