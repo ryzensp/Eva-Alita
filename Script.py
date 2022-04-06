c90c041598a1dc511dcfc99ce0d9d71c80f8e73a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-06 22:11:40 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -275,3 +275,9 @@ Added By - {}
 ID - <code>{}</code>
 Name - {}
 """
+
+
+
+    USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
+    PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
+    PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
