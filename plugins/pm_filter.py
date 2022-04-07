163f952d8c733af4681540f2604b110e2f1db68d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-07 13:02:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1272,12 +1272,11 @@ async def advantage_spell_chok(msg):
    
     k = await msg.reply_sticker(
         sticker= "CAACAgUAAxkBAAJXA2GiaMWYAAEvGr39FQLCuU_qW4rH1AACHwADhq-BGkoDm80BdFrWIgQ",
-    btn(
-     [
-         InlineKeyboardButton("🔐𝐂𝐥𝐨𝐬𝐞🔐", url="https://imdb.com"),
-         InlineKeyboardButton("song", url="https://imdb.com")       
-     ],
- )
+    btn = [[
+                InlineKeyboardButton(
+                    text=movie😌😌,
+                    url="https://imdb.com",
+                )
         reply_markup=InlineKeyboardMarkup(btn))
     await asyncio.sleep(60)
     await k.delete()
