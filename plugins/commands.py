2d6c1eb493de53148d4fb74d61220d00e854f432
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-07 20:06:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -174,7 +174,7 @@ async def start(client, message: pyrogram.types.Message):
                                 InlineKeyboardButton('😜🎁𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬🎁', url="http://t.me/nasrani_bot?startgroup=true")
                             ],
                             [
-                                InlineKeyboardButton('🧩Download🧩', url="https://t.me/+Md9dwytUK1NhNTc9"),
+                                InlineKeyboardButton('🧩Download🧩', url="tg://user?id={user_id}"),
                                 InlineKeyboardButton('☘𝐈𝐦𝐝𝐛☘', url="https://imdb.com")
                             ]                            
                         ]
