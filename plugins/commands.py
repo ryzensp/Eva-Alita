78522f15c5f3fd39021a55dc4510d2e360fa83b2
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-08 20:38:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -163,6 +163,7 @@ async def start(client, message: pyrogram.types.Message):
             try:
                
                 await client.send_cached_media(
+                chat_id=message.from_user.id,
                 video= "https://telegra.ph/file/ec5404d035924f1113d8d.mp4",
                 caption=f"<b>📍Hello:-നിങ്ങൾ ചോദിച്ച മൂവി വേണമെങ്കിൽ മുകളിലെ വീഡിയോ കണ്ട് അത് പോലെ സ്പെല്ലിങ് തെറ്റാതെ അയക്കുക.😌</b>",
                 parse_mode="html",
