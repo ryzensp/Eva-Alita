a54758c03545ff98902735db7a90c79e47877a28
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-08 20:15:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -162,7 +162,7 @@ async def start(client, message: pyrogram.types.Message):
                 f_caption = f"{title}"
             try:
                 await client.send_cached_media(
-                    chat_id=message.from_user.id,
+                    chat_id=MY_CHANNEL,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
