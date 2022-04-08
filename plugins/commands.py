b852b5487fefeacd9cec2c130607b0a03f0ee304
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-08 08:38:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -298,7 +298,7 @@ async def start(client, message: pyrogram.types.Message):
                     k = await client.send_cached_media(
                     chat_id=message.from_user.id,
                     file_id=msg.get("file_id"),
-                    caption={username} <a href='tg://user?id={from_user.id},
+                    caption={username},
                     protect_content=msg.get('protect', False),
                     
                     reply_markup=InlineKeyboardMarkup(
