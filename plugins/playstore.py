748ae3566b194eb921f0db60c50a3f4f57d022ca
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-08 23:15:53 +0530

    Update playstore.py

--- plugins/playstore.py
+++ plugins/playstore.py
@@ -11,12 +11,53 @@ Bot = Client(
     api_hash = os.environ["API_HASH"]
 )
 
+@Client.on_message(filters.command(["app"]) & filters.regex(r'https?://[^\s]+'))
+async def reply_shortens(bot, update):
+    message = await update.reply_text(
+        text="`Analysing your link...`",
+        disable_web_page_preview=True,
+        quote=True
+    )
+    results = play_scraper.search(update.query)
+    answers = []
+    for result in results:
+        details = "**Title:** `{}`".format(result["title"]) + "\n" \
+        "**Description:** `{}`".format(result["description"]) + "\n" \
+        "**App ID:** `{}`".format(result["app_id"]) + "\n" \
+        "**Developer:** `{}`".format(result["developer"]) + "\n" \
+        "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
+        "**Score:** `{}`".format(result["score"]) + "\n" \
+        "**Price:** `{}`".format(result["price"]) + "\n" \
+        "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
+        "**Free:** `{}`".format(result["free"]) + "\n" \
+        "\n" + "Made by @FayasNoushad"
+        reply_markup = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="Play Store", url="https://play.google.com"+result["url"])]]
+        )
+        try:
+            answers.append(
+                InlineQueryResultArticle(
+                    title=result["title"],
+                    description=result.get("description", None),
+                    thumb_url=result.get("icon", None),
+                    input_message_content=InputTextMessageContent(
+                        message_text=details, disable_web_page_preview=True
+                    ),
+                    reply_markup=reply_markup
+                )
+            )
+        except Exception as error:
+            print(error)
+    await update.answer(answers)
 
 
 
 
-@Client.on_message(filters.command('app') & ~filters.private & ~filters.channel)
-async def filter_all(bot, update):
+
+
+
+@Client.on_inline_query()
+async def search(bot, update):
     results = play_scraper.search(update.query)
     answers = []
     for result in results:
