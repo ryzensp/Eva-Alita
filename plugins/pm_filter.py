7dd233bca1dbbb79488e13dbf18be7917dfaec80
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-10 10:42:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1109,7 +1109,7 @@ async def auto_filter(client, msg: pyrogram.types.Message, spoll=False):
                     callback_data=f'{pre}#{file.file_id}#{msg.from_user.id if msg.from_user is not None else 0}',
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
+                    text=f"🐠{get_size(file.file_size)}🐠",
                     callback_data=f'{pre}_#{file.file_id}#{msg.from_user.id if msg.from_user is not None else 0}',
                 )
             ]
@@ -1119,7 +1119,7 @@ async def auto_filter(client, msg: pyrogram.types.Message, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                        text=f"🐠 [{get_size(file.file_size)}] {file.file_name}🐠", 
+                        text=f"🐠 [{get_size(file.file_size)}]🐠{file.file_name}🐠", 
                         callback_data=f'{pre}#{file.file_id}#{msg.from_user.id if msg.from_user is not None else 0}'
                 )
             ] 
