d25f3914b6188c3df5de89024bf3f8f05d5c43b4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-11 23:45:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/200920082007/TigerShroff.git /TigerShroff
+  git clone https://github.com/Azanpopz/Eva-Alita.git /Eva-Alita
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Tigershroff
+  git clone $UPSTREAM_REPO /Eva-Alita
 fi
 cd /Tigershroff
 pip3 install -U -r requirements.txt
