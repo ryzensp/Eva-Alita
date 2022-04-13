4169c48552ba7d77619c93655731105acf0e26f2
cshowl <86688030+cshowl@users.noreply.github.com>
2022-04-14 01:46:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Azanpopz/Eva-Alita.git /Eva-Alita
+  git clone https://github.com/cshowl/Eva-Alita.git /Eva-Alita
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva-Alita
