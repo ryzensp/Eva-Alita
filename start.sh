76b6fa1fc04f47a6885113866af983bddc0ae611
cshowl <86688030+cshowl@users.noreply.github.com>
2022-04-14 01:58:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva-Alita
 fi
-cd /Tigershroff
+cd /Eva-Alita
 pip3 install -U -r requirements.txt
 echo "Starting TIGER Shroff....🔥"
 python3 bot.py
